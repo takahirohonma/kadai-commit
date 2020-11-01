91d2e91a582df1e628d95472a35c51a8c8201602
takahiro.honma <takahiro.ryuikihonma3776@gmail.com>
2020-11-01 10:54:44 +0000

    add sample.php

---
+++ sample.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>おみくじ</title>
+    </head>
+    <body>
+        <h1>おみくじ</h1>
+        <p>おみくじの結果:なし</p>
+        <form action="sample.php" method="POST">
+            <buttom type="submit" name="act" value="draw">おみくじをひく！</buttom>
+        </form>
+    </body>
+</html>
