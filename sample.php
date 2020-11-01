d2966d648e16952cf9463845a9756b3170b95196
takahiro.honma <takahiro.ryuikihonma3776@gmail.com>
2020-11-01 13:45:00 +0000

    update sample.php

--- sample.php
+++ sample.php
@@ -1,3 +1,7 @@
+<?php
+    $result ="無し。";
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -6,9 +10,9 @@
     </head>
     <body>
         <h1>おみくじ</h1>
-        <p>おみくじの結果:なし</p>
+        <p>おみくじの結果:<?php print htmlspecialchars($result, ENT_QUOTES, "UTF-8"); ?></p>
         <form action="sample.php" method="POST">
-            <buttom type="submit" name="act" value="draw">おみくじをひく！</buttom>
+            <button type="submit" name="act" value="draw">おみくじをひく！</button>
         </form>
     </body>
 </html>
